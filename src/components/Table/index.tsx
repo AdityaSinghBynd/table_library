bd8341774001dd3f80becbdee3b5d902060d86ce
AdityaSinghBynd <aditya@bynd.ai>
2025-02-18 02:28:14 +0530

    Project Setup

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import TableProps  from '../../interface/table.interface';
+
+export default function Table({}: TableProps) {
+
+    return(
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        Table Header
+                    </tr>
+                </thead>
+            </table>
+        </>
+    )
+}
+
